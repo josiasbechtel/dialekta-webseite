8c71fb4d6a6901b81b64a4b37b2bc8846ffb3d90
josiasbechtel <josias.bechtel@dialekta.ch>
2026-06-08 15:25:20 +0200

    Add impressum page

---
+++ app/impressum/page.tsx
@@ -0,0 +1,3 @@
+export default function ImpressumPage() {
+  return <main className="page"><section className="section"><div className="container card"><h1>Impressum</h1><p>Dialekta GmbH</p><p>CHE-338.544.623</p><p>E-Mail: info@dialekta.ch</p><p>Telefon Demoagent: +41 61 539 19 29</p></div></section></main>
+}
